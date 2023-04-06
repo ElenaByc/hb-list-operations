56d60526b85069c9051b97f30a9aca357d0f1f67
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-06 09:16:57 -0700

    feat: solve basic 16 problems

--- list_operations.py
+++ list_operations.py
@@ -12,7 +12,7 @@ def head(input_list):
       'Jan'
     """
 
-    return []
+    return input_list[0]
 
 
 def tail(input_list):
@@ -25,7 +25,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -38,7 +38,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
 
 
 def top(input_list):
@@ -51,7 +51,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[0:-1]
 
 
 def first_three(input_list):
@@ -64,7 +64,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -77,7 +77,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -90,7 +90,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -103,7 +103,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -119,7 +119,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -135,8 +135,7 @@ def replace_head(input_list):
     [42, 3, 6, 9, 12, 15, 18, 21, 24, 27]
 
     """
-
-    pass
+    input_list[0] = 42
 
 
 def replace_third_and_last(input_list):
@@ -152,8 +151,8 @@ def replace_third_and_last(input_list):
     [0, 3, 37, 9, 12, 15, 18, 21, 24, 37]
 
     """
-
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
 
 
 def backwards(input_list):
@@ -168,7 +167,7 @@ def backwards(input_list):
 
     """
 
-    pass
+    return input_list[::-1]
 
 
 def every_other(input_list):
@@ -181,7 +180,7 @@ def every_other(input_list):
 
     """
 
-    pass
+    return input_list[::2]
 
 
 def delete_third_and_seventh(input_list):
@@ -198,7 +197,8 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    pass
+    del input_list[6]
+    del input_list[2]
 
 
 ### List Iteration Problems. Built-in methods are allowed for these! ###
@@ -213,7 +213,13 @@ def indices_of_positive_numbers(input_list):
 
     """
 
-    pass
+    result_list = []
+
+    for i in range(len(input_list)):
+        if input_list[i] > 0:
+            result_list.append(i)
+    
+    return result_list
 
 
 def sum_repeats(input_list):
@@ -229,4 +235,12 @@ def sum_repeats(input_list):
 
     """
 
-    pass
+    sum = 0
+    num = input_list[0]
+    for i in range(1, len(input_list)):
+        if input_list[i] == num:
+            sum += num
+        else:
+            num = input_list[i]
+
+    return sum
